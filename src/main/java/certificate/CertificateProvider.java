38c82549eeb31710b65a1cb167bc53967bba4541
Ernest Cui <ecuiships@126.com>
2022-11-02 23:16:40 +0800

    added docs for certificate interfaces

--- src/main/java/certificate/CertificateProvider.java
+++ src/main/java/certificate/CertificateProvider.java
@@ -1,7 +1,21 @@
 package certificate;
 
 public interface CertificateProvider {
+    /**
+     * 提供证书字节序列。
+     *
+     * @return  UTF-8编码的证书字节序列。
+     */
     byte[] getCertificate();
+
+    /**
+     * 使用证书私钥对通信字节序列进行签名。
+     *
+     * @param   traffic
+     *          要被签名的通信数据字节序列。
+     *
+     * @return  签名结果的字节序列。
+     */
     byte[] signTraffic(byte[] traffic);
 
     static CertificateProvider getInstance(){
--- src/main/java/certificate/CertificateValidator.java
+++ src/main/java/certificate/CertificateValidator.java
@@ -1,7 +1,30 @@
 package certificate;
 
 public interface CertificateValidator {
+    /**
+     * 验证证书合法性。
+     *
+     * @param   certificate
+     *          UTF-8编码的证书字节序列。
+     *
+     * @return  返回{@code true}如果验证通过，否则返回{@code false}。
+     */
     boolean validateCertificate(byte[] certificate);
+
+    /**
+     * 验证通信数据签名的合法性。
+     *
+     * @param   certificate
+     *          UTF-8编码的证书字节序列。
+     *
+     * @param   traffic
+     *          被签名的通信数据字节序列。
+     *
+     * @param   trafficSignature
+     *          要被验证的签名的字节序列。
+     *
+     * @return  返回{@code true}如果验证通过，否则返回{@code false}。
+     */
     boolean validateTrafficSignature(byte[] certificate,byte[] traffic, byte[] trafficSignature);
 
     static CertificateValidator getInstance(){
