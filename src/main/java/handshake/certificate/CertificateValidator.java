432a4063298ea31ffcd290ce0c7b2eacfcde31ea
Ernest Cui <ecuiships@126.com>
2022-11-30 21:10:17 +0800

    CertificateValidator doc modified

--- src/main/java/handshake/certificate/CertificateValidator.java
+++ src/main/java/handshake/certificate/CertificateValidator.java
@@ -13,7 +13,7 @@ public interface CertificateValidator {
      * @param   host
      *          UTF-8编码的当前客户端正在访问的主机名，例如192.168.3.254:8080。
      *
-     * @return  返回{@code true}如果验证通过，否则返回{@code false}。
+     * @return  返回一个{@code CertificateValidationResult}对象，包含验证是否通过以及如果失败的错误信息。
      */
     CertificateValidationResult validateCertificate(byte[] certificate, String host);
 
