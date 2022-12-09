711a71bcc4b3e62e89077d24ab122a4d4db10d19
Ernest Cui <ecuiships@126.com>
2022-12-09 10:28:37 +0800

    improved error page style

--- src/main/java/config/clientimpl/ClientConfigManager.java
+++ src/main/java/config/clientimpl/ClientConfigManager.java
@@ -4,43 +4,54 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClientConfigManager {
     private static ClientProxyConfig config;
 
     public static void load(String configFilePath) throws IOException {
-        ObjectMapper objectMapper=new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        String configJsonContent= Files.readString(
+        String configJsonContent = Files.readString(
                 Path.of(configFilePath), StandardCharsets.UTF_8);
 
         config = objectMapper.readValue(configJsonContent, ClientProxyConfig.class);
 
-        String propertyMissingTemplate="property '%s' missing from client config file";
+        String propertyMissingTemplate = "property '%s' missing from client config file";
 
-        if(config.getPort()==null){
+        if (config.getPort() == null) {
             throw new RuntimeException(propertyMissingTemplate.formatted("port"));
         }
 
-        if(config.getTimeout()==null){
+        if (config.getTimeout() == null) {
             throw new RuntimeException(propertyMissingTemplate.formatted("timeout"));
         }
 
-        if(config.getTargetHostPatterns()==null){
+        if (config.getTargetHostPatterns() == null) {
             throw new RuntimeException(propertyMissingTemplate.formatted("targetHostPatterns"));
         }
     }
 
-    public static Integer getPort(){
+    public static Integer getPort() {
         return config.getPort();
     }
 
-    public static Integer getTimeout(){
+    public static Integer getTimeout() {
         return config.getTimeout();
     }
 
-    public static boolean isTargetHost(String host){
+    public static boolean isTargetHost(String host) {
         return config.getTargetHostPatterns().stream().anyMatch(host::matches);
+    }
+
+    public static String getTargetHostPatternsHtmlText() {
+        StringBuilder builder = new StringBuilder();
+
+        for (var i : config.getTargetHostPatterns()) {
+            builder.append(i).append("<br/>");
+        }
+
+        return builder.toString();
     }
 }
